1c71288b7c25abf70bdfdf55b4fc1446515c8d8d
amir khaled <livebox07@live.fr>
2020-01-10 11:16:12 +0100

    early draft

---
+++ unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum GameState  {MainMenu = 0,Playing = 1, Pause = 2}
+public enum GameMode  {Easy = 0,Hard = 1}
+public enum GamePhase  {Phase_1 = 0, Phase_2 = 1, Phase_3 = 2 }
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public GameState CurrentState;
+    public GameMode CurrentMode;
+    public GamePhase CurrentPhase;
+    public List<SituationObject> SituationList;
+    public TimeLineManager TimeLine;
+
+    void Awake()
+    { 
+        Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetMainMenu();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void SetMainMenu()
+    {
+        SetState(0);
+
+    }
+
+    private void SetState(int State)
+    {
+        CurrentState = (GameState)State;
+    }
+
+}
+
---
+++ unity/Assets/Scripts/SituationObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+[CreateAssetMenu]
+public class SituationObject : ScriptableObject
+{
+    public string Name;
+
+    //[Header("Phase 1")] 
+    //[Range(0,1)]
+    //public float speedPhase1;
+    //public float playTimePhase1;
+    [SerializeField]
+    public Phase Phase1, Phase2, Phase3;
+
+}
+
+[Serializable]
+public struct Phase
+{
+    [Range(0, 1)]
+    public float speedPhase1;
+    public float playTimePhase1;
+    public VideoClip clip;
+}
